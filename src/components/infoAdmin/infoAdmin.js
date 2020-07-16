357bd449878e8396681397c3212d628683918a20
RAJATsd <rajatsharmaatri@gmail.com>
2020-07-16 15:41:05 +0530

    made basic ui for notifications

--- src/components/infoAdmin/infoAdmin.js
+++ src/components/infoAdmin/infoAdmin.js
@@ -1,19 +1,65 @@
 import React from 'react';
 import io from 'socket.io-client';
 import Container from 'react-bootstrap/Container';
+import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 
 class InfoAdmin extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            socket:null
+            socket:null,
+            notifications:[],
+            open:false,
+            unread:0
         };
     }
 
     componentDidMount(){
         this.initSocket();
+        this.getAllNotifications();
+    }
+
+    getAllNotifications = () => {
+        //console.log('i am in this');
+        axios.get('http://localhost:8080/contractor/allNotifications')
+        .then(response=>{
+            console.log(response.data.data);
+            const notifications = response.data.data;
+            let unread = 0;
+            for(let ntf of response.data.data){
+                if(ntf.isRead===0){
+                    unread = unread+1 ;
+                }
+            }
+            this.setState({notifications:notifications,unread:unread}); 
+        })
+        .catch(err=>{
+            console.log(err)
+        });
+    }
+
+    changeUnreadToRead = () => {
+        axios.get('http://localhost:8080/contractor/changeNotStatus')
+        .then(response=>{
+            if(response.data.success){
+                let unread = 0;
+                this.setState({unread:unread});
+            }
+        })
+        .catch(e=>console.log(e));
+    }
+
+    showOrHide = (event) => {
+        let {open} = this.state;
+        open = !open;
+        if(open && this.state.unread!=0)
+        {
+            this.changeUnreadToRead();
+        }
+        this.setState({open:open});
     }
 
     initSocket =()=>{
@@ -22,6 +68,17 @@ class InfoAdmin extends React.Component{
         console.log('initialised socket on frontend');
         socket.on('outside',(args)=>{
             console.log(args);
+            if(!this.state.open){
+                let {unread} = this.state;
+                unread = unread+1;
+                this.setState({unread:unread});
+            }
+            else{
+                this.changeUnreadToRead();
+            }
+            let {notifications} = this.state;
+            notifications.push(args.notification);
+            this.setState({notifications:notifications});
         });
         // socket.on('new message',(args)=>{
         //     console.log(args);
@@ -40,8 +97,9 @@ class InfoAdmin extends React.Component{
 
     render(){
         return(
-            <Container fluid>
-                
+            <Container className="align-items-center">
+                <Button variant="secondary" onClick={this.showOrHide}>Notifications-{this.state.unread} unread</Button>
+                {this.state.open && this.state.notifications.map((nofify,i)=><Alert variant="dark" key={i}>{nofify.notificationText}</Alert>)}
             </Container>
         )
     }
